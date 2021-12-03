29f2577331a99d2acc6999ad6075c3ba0dde3425
Edgar Albalate <dev.gardo@gmail.com>
2021-12-03 21:50:30 +0100

    Ended day one

--- day01/__init__.py
+++ day01/__init__.py
@@ -1,9 +1,12 @@
 
 
+def parseNumbers(input: str) -> [str]:
+    return [int(num) for num in input.strip().split("\n")]
 
-def main(input: str):
 
-    numbers = [int(num) for num in input.strip().split("\n")]
+def part1(input: str):
+
+    numbers = parseNumbers(input)
 
     last = None
     increased = 0
@@ -22,7 +25,26 @@ def main(input: str):
     print(f"(Decreased: {decreased}")
 
 
+def part2(input: str):
 
+    numbers = parseNumbers(input)
+    groups = [numbers[i] + numbers[i+1] + numbers[i+2] for i in range(len(numbers) -2)]
+
+    last = groups[0]
+    increased = 0
+    decreased = 0
+    equals = 0
+
+    for group in  groups[1:]:
+        if last < group: increased += 1
+        elif last > group: decreased += 1
+        else: equals += 1
+        last = group
+    print(increased)
+    print(decreased)
+    print(equals)
+def main(input: str):
+    part2(input)
 
 if __name__ == "__main__":
     from sys import argv
